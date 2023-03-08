0e115e248929d70addf7a03b6fa8dd1992535ebe
Floriane Davadie <floriane.davadie@laposte.net>
2023-03-08 16:22:21 +0100

    calendar

---
+++ app/controllers/calendars_controller.rb
@@ -0,0 +1,10 @@
+class CalendarsController < ApplicationController
+
+  def index
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    # Or, for a weekly view:
+    @meetings = Meeting.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
+  end
+end
